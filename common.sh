38d0fb912e71d6357a5da362453123cba24cd8aa
Tyler Flint <tylerflint@gmail.com>
2015-07-03 13:33:48 -0600

    adding validation helpers

--- common.sh
+++ common.sh
@@ -2,9 +2,6 @@
 # vim: ts=2 sw=2 ft=bash noet
 
 # TODO:
-# validate_presence -> ensure a key exists
-# validate_in -> ensure a key has a value within a list of options
-# validate_not_in -> ensure a key does not have a value within a list of options
 # set_evar -> set an environment variable for use in running container
 # whitelist_evars -> sets evars from the payload evars that match a whitelist
 # blacklist_evars -> sets evars from the payload evars that don't match a blacklist
--- lib/helpers.sh
+++ lib/helpers.sh
@@ -9,3 +9,91 @@
 install() {
   pkgin -y in $1
 }
+
+
+# validate_presence(1)
+# 
+# $1 = field
+# 
+# Validate that a field exists within the boxfile payload and has a value.
+# If the check fails, a fatal error will be printed an a non-zero exit
+# will be forced.
+validate_presence() {
+  if [ -z $(payload "$1") ]; then
+    label=${1/boxfile_build/}
+    print_fatal "value required" "Boxfile 'build' section requires ${label} to exist and have a value to continue"
+    exit 1
+  fi
+}
+
+# validate_in(2)
+# 
+# $1 = field
+# $2 = possible values
+# 
+# Validates that if a field exists, the value present falls within a range
+# of acceptable options.
+validate_in() {
+  field=$1
+  options=$2
+  pass=0
+
+  for option in $options; do
+    if [ "$(payload "${field}")" = "$option" ]; then
+      pass=1
+    fi
+  done
+
+  if [ $pass = 0 ]; then
+    label=${field/boxfile_build/}
+    message_opts=""
+
+    for option in $options; do
+      if [ -n $message_opts ]; then
+        message_opts="${message_opts}, "
+      fi
+      message_opts="${message_opts}${option}"
+    done
+
+    message="Boxfile 'build' section attribute $label value must be one of the following: ${message_opts}"
+
+    print_fatal "invalid value", "${message}"
+    exit 1
+  fi
+}
+
+# validate_not_in(2)
+# 
+# $1 = field
+# $2 = possible values
+# 
+# Validates that if a field exists, the value present falls does not fall
+# within a range of options.
+validate_not_in() {
+  field=$1
+  options=$2
+  pass=1
+
+  for option in $options; do
+    if [ "$(payload "${field}")" = "$option" ]; then
+      pass=0
+    fi
+  done
+
+  if [ $pass = 0 ]; then
+    label=${field/boxfile_build/}
+    message_opts=""
+
+    for option in $options; do
+      if [ -n $message_opts ]; then
+        message_opts="${message_opts}, "
+      fi
+      message_opts="${message_opts}${option}"
+    done
+
+    message="Boxfile 'build' section attribute $label value must be one of the following: ${message_opts}"
+
+    print_fatal "invalid value", "${message}"
+    exit 1
+  fi
+}
