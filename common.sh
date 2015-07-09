6c7405e3983349b81faa4ead07ddc1564074b65b
Tyler Flint <tylerflint@gmail.com>
2015-07-09 13:06:59 -0600

    adding functions to assist with environment variables

--- common.sh
+++ common.sh
@@ -1,20 +1,11 @@
 # -*- mode: bash; tab-width: 2; -*-
 # vim: ts=2 sw=2 ft=bash noet
 
-# TODO:
-# set_evar -> set an environment variable for use in running container
-# whitelist_evars -> sets evars from the payload evars that match a whitelist
-# blacklist_evars -> sets evars from the payload evars that don't match a blacklist
-# 
-# NEED TO FIGURE OUT:
-# templating
-# simple find/replace within existing file
-
 lib_dir="$(dirname $BASH_SOURCE)/lib"
 
 # include components
 . ${lib_dir}/evars.sh
-. ${lib_dir}/helpers.sh
 . ${lib_dir}/hooks.sh
 . ${lib_dir}/payload.sh
 . ${lib_dir}/output.sh
+. ${lib_dir}/helpers.sh
--- lib/evars.sh
+++ lib/evars.sh
@@ -1,3 +1,40 @@
 # -*- mode: bash; tab-width: 2; -*-
 # vim: ts=2 sw=2 ft=bash noet
 
+# set_evar(2)
+# 
+# $1 = key
+# $2 = value
+# 
+# Sets an environment variable which will be available to the process that
+# the code runs in after a successful deploy.
+set_evar() {
+  env_dir=$(payload 'env_dir')
+  echo $2 > ${env_dir}/$1
+}
+
+# whitelist_evars(1)
+# 
+# $1 = whitelist regex
+# 
+# Sets environment variables which will be available to the process that
+# the code runs in, from the payload, according the the whitelist.
+whitelist_evars() {
+  evars=(${PL_env_nodes//,/ })
+  for evar in "${evars[@]}"; do
+    echo $evar | grep -E $1 && set_evar $evar $(payload "env_${evar}")
+  done
+}
+
+# blacklist_evars(1)
+# 
+# $1 = blacklist regex
+# 
+# Sets environment variables which will be available to the process that
+# the code runs in, from the payload, according the the blacklist.
+blacklist_evars() {
+  evars=(${PL_env_nodes//,/ })
+  for evar in "${evars[@]}"; do
+    echo $evar | grep -E $1 || set_evar $evar $(payload "env_${evar}")
+  done
+}
