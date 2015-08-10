0a947b39d455c8f550a3068f45b0a2f545ca23a1
Tyler Flint <tylerflint@gmail.com>
2015-08-10 13:06:08 -0600

    adding helpers to render templates and files; refactoring common to assist in rendering

--- common.sh
+++ common.sh
@@ -1,11 +1,38 @@
 # -*- mode: bash; tab-width: 2; -*-
 # vim: ts=2 sw=2 ft=bash noet
 
-lib_dir="$(dirname $BASH_SOURCE)/lib"
+# initialize globals, to be set during nos_init
+NOS_bin_script=""
+NOS_engine_root=""
+NOS_template_dir=""
+NOS_file_dir=""
+NOS_lib_dir=""
 
 # include components
-. ${lib_dir}/evars.sh
-. ${lib_dir}/hooks.sh
-. ${lib_dir}/payload.sh
-. ${lib_dir}/output.sh
-. ${lib_dir}/helpers.sh
+lib_dir="$(dirname $BASH_SOURCE)/lib"
+components=( evars hooks payload output helpers render)
+for component in "${components[@]}"
+do
+  . ${lib_dir}/${component}.sh
+done
+
+# nos_init(2)
+#
+# $0 = original filename
+# $1 = original payload
+#
+# Initialized nos with the arguments provided to the original script
+nos_init() {
+  # pwd should be the bin script directory!
+  current_dir=$(pwd)
+
+  # initialize globals
+  NOS_bin_script="${current_dir}/$(basename $0)"
+  NOS_engine_root="${current_dir}/../"
+  NOS_template_dir="${current_dir}/../templates"
+  NOS_file_dir="${current_dir}/../files"
+  NOS_lib_dir="${current_dir}/../lib"
+
+  # initialize payload
+  eval_payload $1
+}
---
+++ lib/render.sh
@@ -0,0 +1,22 @@
+
+# template(3)
+#
+# $1 = source
+# $2 = destination
+# $3 = JSON|YAML payload
+#
+# Renders a mustache template from the templates directory and persists the
+# result at (destination). The payload provided is passed to mustache.
+template() {
+  echo "${3}" | mustache - ${NOS_template_dir}/${1} > $2
+}
+
+# file(2)
+#
+# $1 = source
+# $2 = destination
+#
+# Copies a file from the files directory to the specified destination.
+file() {
+  cp -f ${NOS_file_dir}/${1} $2
+}
