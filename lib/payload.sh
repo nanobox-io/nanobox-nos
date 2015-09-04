38b0baf9f5ba61d2789f922a88d0761c42728a62
Tyler Flint <tylerflint@gmail.com>
2015-09-04 16:04:38 -0600

    reverting to a simpler model

--- lib/payload.sh
+++ lib/payload.sh
@@ -17,34 +17,14 @@ eval_payload() {
 payload() {
   type="PL_${1}_type"
 
-  if [[ "$type" = "map" ]]; then
-    eval "PL_${1}_value=()"
-    nodes_var="PL_${1}_nodes"
-    list=${!nodes_var}
-    nodes=(${list}//,/ })
-    for node in "${nodes[@]}"; do
-      node_var="PL_${1}_${node}_value"
-      key=node
-      val=${!node_var}
-      eval "PL_${1}_value+=(\"${key}=${val}\")"
-    done
-    echo "PL_${1}_value"
-
-  elif [[ "$type" = "array" ]]; then
-    >&2 echo "setting: PL_${1}_value=()"
-    eval "PL_${1}_value=()"
-    len_var="PL_${1}_length"
-    length=${!len_var}
+  if [[ "$type" = "array" ]]; then
+    list=()
     for (( i=0; i < $length; i++ ))
     do
       val="PL_${1}_${i}_value"
-      eval "PL_${1}_value+=(\"${!val}\")"
-    done
-    var="PL_${1}_value"
-    val=${!var}
-    >&2 echo "PL_${1}_value=${val[@]}"
-    echo "PL_${1}_value"
-
+      list+=("${!val}")
+    done    
+    echo "${list[@]}"
   else
     val="PL_${1}_value"
     echo "${!val}"  
