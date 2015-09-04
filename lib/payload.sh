820186320b791ed02a06df3737ffce40d479554a
Tyler Flint <tylerflint@gmail.com>
2015-09-04 15:03:00 -0600

    expanding the payload helper to fetch arrays and maps

--- lib/payload.sh
+++ lib/payload.sh
@@ -15,6 +15,36 @@ eval_payload() {
 # 
 # A simple getter for fetching a payload value
 payload() {
-  val="PL_${1}_value"
-  echo "${!val}"
+  type="PL_${1}_type"
+
+  case "${!type}" in
+    "map" )
+      eval "PL_${1}_value=()"
+      nodes_var="PL_${1}_nodes"
+      list=${!nodes_var}
+      nodes=(${list}//,/ })
+      for node in "${nodes[@]}"; do
+        node_var="PL_${1}_${node}_value"
+        key=node
+        val=${!node_var}
+        eval "PL_${1}_value+=(\"${key}=${val}\")"
+      done
+      echo "PL_${1}_value"
+      ;;
+    "array" )
+      eval "PL_${1}_value=()"
+      len_var="PL_${1}_length"
+      length=${!len_var}
+      for (( i=0; i < $length; i++ ))
+      do
+        val="PL_${1}_${i}_value"
+        eval "PL_${1}_value+=(\"${!val}\")"
+      done
+      echo "PL_${1}_value"
+      ;;
+    * )
+      val="PL_${1}_value"
+      echo "${!val}"      
+      ;;
+  esac
 }
