64419f8f9bb078723e5763e5fa18a33a5cb8ef5f
Braxton Huggins <braxton@pagodabox.com>
2015-10-27 15:26:20 -0600

    fix run hooks, redirect stderr to stdout so it can be captured

--- tests/functions/run.sh
+++ tests/functions/run.sh
@@ -7,21 +7,21 @@
 MD5_COMMAND=$(which md5sum || echo md5)
 
 # run_process
-out=$(run_process "hello"   "true")
+out=$(run_process "hello"   "true" 2>&1)
 echo "$out" | grep HELLO &> /dev/null || (echo "TEST ('run_proc hello') FAILED!"; false)
 
 # run_subprocess false
-out=$(run_subprocess "exit bad"   "false")
+out=$(run_subprocess "exit bad"   "false" 2>&1)
 echo "$out" | grep -i failed &> /dev/null || (echo "TEST ('run_sub exit bad') FAILED!"; false)
 
 # run_subprocess true
-out=$(run_subprocess "exit clean" "true")
+out=$(run_subprocess "exit clean" "true" 2>&1)
 echo "$out" | grep -i succ &> /dev/null || (echo "TEST ('run_sub exit clean') FAILED!"; false)
 
 # run_hooks string
 set_evar 'PL_boxfile_before_exec_type' 'string'
 set_evar 'PL_boxfile_before_exec_value' 'echo "hola gato"'
-out=$(run_hooks 'before' | $MD5_COMMAND | cut -f 1 -d ' ')
+out=$(run_hooks 'before' 2>&1 | $MD5_COMMAND | cut -f 1 -d ' ')
 if [[ "${out}" != "e6533fb0e60eccf7290b2c8524be1437" ]]; then
   echo "TEST ('run_hooks string') FAILED!"
   false
@@ -34,7 +34,7 @@ set_evar 'PL_boxfile_after_exec_0_type' 'string'
 set_evar 'PL_boxfile_after_exec_0_value' 'true'
 set_evar 'PL_boxfile_after_exec_1_type' 'string'
 set_evar 'PL_boxfile_after_exec_1_value' 'false'
-out=$(run_hooks 'after' | $MD5_COMMAND | cut -f 1 -d ' ')
+out=$(run_hooks 'after' 2>&1 | $MD5_COMMAND | cut -f 1 -d ' ')
 if [[ "${out}" != "7547b06ad88a8c1c050e6764a1a73c4a" ]]; then
   echo "TEST ('run_hooks array') FAILED!"
   false
