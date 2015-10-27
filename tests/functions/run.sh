97d47eb922424481669254b45eb39f7765198910
Braxton Huggins <braxton@pagodabox.com>
2015-10-27 15:32:43 -0600

    fix expected output on run tests

--- tests/functions/run.sh
+++ tests/functions/run.sh
@@ -22,8 +22,8 @@ echo "$out" | grep -i succ &> /dev/null || (echo "TEST ('run_sub exit clean') FA
 set_evar 'PL_boxfile_before_exec_type' 'string'
 set_evar 'PL_boxfile_before_exec_value' 'echo "hola gato"'
 out=$(run_hooks 'before' 2>&1 | $MD5_COMMAND | cut -f 1 -d ' ')
-if [[ "${out}" != "e6533fb0e60eccf7290b2c8524be1437" ]]; then
-  echo "TEST ('run_hooks string') FAILED!"
+if [[ "${out}" != "da6c1da50cf6a118c89b1fa872a0ae70" ]]; then
+  echo "TEST ('run_hooks string') FAILED! Got ${out}"
   false
 fi
 
@@ -35,8 +35,8 @@ set_evar 'PL_boxfile_after_exec_0_value' 'true'
 set_evar 'PL_boxfile_after_exec_1_type' 'string'
 set_evar 'PL_boxfile_after_exec_1_value' 'false'
 out=$(run_hooks 'after' 2>&1 | $MD5_COMMAND | cut -f 1 -d ' ')
-if [[ "${out}" != "7547b06ad88a8c1c050e6764a1a73c4a" ]]; then
-  echo "TEST ('run_hooks array') FAILED!"
+if [[ "${out}" != "5f3f681f93de9b6de659fddd5c18e340" ]]; then
+  echo "TEST ('run_hooks array') FAILED! Got ${out}"
   false
 fi
 
