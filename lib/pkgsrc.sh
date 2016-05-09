0d3ee0fe1d6785432498980b779610df32a68dc8
Braxton Huggins <braxton@pagodabox.com>
2016-05-09 11:20:05 -0600

    Update label on installing/uninstalling packages to not have list of packages, if it was more than 40 characters, it was getting stuck

--- lib/pkgsrc.sh
+++ lib/pkgsrc.sh
@@ -7,7 +7,7 @@
 #
 # A simple expressive shortcut to install a pkgsrc package via pkgin
 nos_install() {
-  nos_run_subprocess "installing $(nos_package_label "$@")" "pkgin -y in $(echo $@)"
+  nos_run_subprocess "installing packages" "pkgin -y in $(echo $@)"
 }
 
 # nos_uninstall(1+)
@@ -16,40 +16,5 @@ nos_install() {
 #
 # A simple expressive shortcut to uninstall a pkgsrc package via pkgin
 nos_uninstall() {
-  nos_run_subprocess "uninstalling $(nos_package_label "$@")" "pkgin -y rm $(echo $@)"
-}
-
-# nos_package_label(1+)
-#
-# $@ = list of packages by name
-#
-# Returns a string of packages separated by commas, with an "and" before
-# the last item
-nos_package_label() {
-  count=$#
-  label=""
-
-  if [[ $count -eq 1 ]]; then
-    label=$1
-  else
-    i=1
-    for arg in "$@"; do
-      # first?
-      if [[ $i -eq 1 ]]; then
-        label=$arg
-      # two?
-      elif [[ $i -eq $count ]] && [[ $count -eq 2 ]]; then
-        label="${label} and ${arg}"
-      # last?
-      elif [[ $i -eq $count ]]; then
-        label="${label}, and ${arg}"
-      # middle ?
-      else
-        label="${label}, ${arg}"
-      fi
-      let i++
-    done
-  fi
-
-  echo $label
+  nos_run_subprocess "uninstalling packages" "pkgin -y rm $(echo $@)"
 }
